e294224a23bca7b77f6b5e85408e009ddf07b7de
Mahesh9213 <maheshbonthada9866@gmail.com>
2025-07-18 12:50:31 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // ✅ This is the fix for blank screen on Render
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
